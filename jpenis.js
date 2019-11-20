ce8e6c3ed3643cb1b213c74528d42b21bc035d0d
pabble <49900766+pabble@users.noreply.github.com>
2019-11-20 10:09:04 +0100

    v0.0.1

--- jpenis.js
+++ jpenis.js
@@ -1,1 +1,16 @@
-/* jPenis v0.0.1 | (c) 2019 Pabble Studios, Inc. | https://pabble.github.io/ */
+/* jPenis v1.0 | (c) 2019 Pabble Studios, Inc. | https://pabble.github.io/ */
+
+function createVector(x, y){
+    return {
+        x: x,
+        y: y
+    };
+}
+
+function createVector3(x, y, z){
+    return {
+        x: x,
+        y: y,
+        z: z
+    };
+}
